c8738e2364c57c59fccf5c294b456b771852c859
Marco <gilles.marco06@gmail.com>
2019-10-09 09:48:51 +0200

    Canvas to fullscreen

--- script.js
+++ script.js
@@ -3,8 +3,6 @@ window.onload = init;
 function init(){
   //Resize canvas to fullscreen
   var canvas = document.querySelector("#myCanvas");
-  canvas.height = document.innerHeight;
-  canvas.width = document.innerWidth;
   
   //Start animation
   requestAnimationFrame(updateCanvas);
