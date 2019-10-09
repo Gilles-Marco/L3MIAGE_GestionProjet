5e3f15ae9e01ee7e70f62f424b61174470506678
Gilles-Marco <36821430+Gilles-Marco@users.noreply.github.com>
2019-10-09 09:03:24 +0200

    Update script.js
    animation's loop creation

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 window.onload = init;
 
 function init(){
+  //Resize canvas to fullscreen
   var canvas = document.querySelector("#myCanvas");
   canvas.height = document.innerHeight;
   canvas.width = document.innerWidth;
+  
+  //Start animation
+  requestAnimationFrame(updateCanvas);
+}
+
+function updateCanvas(){
+  
+  requestAnimationFrame(updateCanvas);
 }
