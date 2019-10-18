e403673d88e83b22281964e93c2e75a526e33f08
Marco <gilles.marco06@gmail.com>
2019-10-18 21:11:31 +0200

    Line clipping to finish

--- ennemy.js
+++ ennemy.js
@@ -12,11 +12,27 @@ export class Ennemi{
         this.jumpMode = false;
         this.compteur = 0;
 
+        this.nbOfTurn = 10;
         this.futureHit = false;
         this.colliding = false;
 
         this.vy = 0;
-        this.VYMAX = 0.2;
+        this.VYMAX = 15;
+    }
+
+    intersect(vector){
+        /**
+         * Return true if the ennemy is intersected by the vector
+         * return false if not
+         */
+        //TODO
+        let targetx1 = this.x-this.width/2;
+        let targetx2 = this.x+this.width/2;
+        let targety1 = this.y;
+        let targety2 = this.y+this.height;
+        if()
+        else
+            return false;
     }
 
     isHittingMe(arrowArray){
@@ -25,29 +41,21 @@ export class Ennemi{
          * change the value of futureHit to true, or false && vy to +0.2
          */
         for(let i=0;i<arrowArray.length;i++){
-            if(true){
+            //Test if the arrow is behind him
+            let goingToHitYou = false;
+            if(arrowArray[i].x>this.x);
+            
+            //Test if the vector of the arrow is going to hit the target
+            let vector = {"x1":arrowArray[i].x, "y1":arrowArray[i].y, "x2":arrowArray[i].x+arrowArray[i].vx*this.nbOfTurn, "y2":arrowArray[i].y+arrowArray[i].vy*this.nbOfTurn};
+            if(this.intersect(vector))
+                goingToHitYou = true;
+
+            if(goingToHitYou){
                 this.futureHit = true;
                 this.vy = this.VYMAX;
                 break;
             }
         }
-    }
-
-    collide(platformArray){
-        /**
-         * Detect if he collide with a platform
-         * change the value of this.futureHit to true, or false
-         */
-        let bottomX = this.x+this.height/2;
-
-        for(let i=0;i<platformArray.length;i++){
-            if(true){
-                this.vy = this.VYMAX;
-                this.colliding = true;
-                break;
-            }
-        }
-        
     }
 
     update(){
@@ -74,6 +82,8 @@ export class Ennemi{
         this.update();
 
         //Draw the ennemy
+        this.ctx.fillStyle = "red";
+        this.ctx.fillRect(this.x-this.width/2, this.y, this.width, this.height);
 
         this.ctx.restore();
     }
