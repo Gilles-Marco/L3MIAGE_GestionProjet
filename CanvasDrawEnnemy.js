d3abc69933c296f605241fd480cf3eb8061a2359
Gilles-Marco <36821430+Gilles-Marco@users.noreply.github.com>
2019-11-14 07:57:32 +0100

    Create CanvasDrawEnnemy.js

---
+++ CanvasDrawEnnemy.js
@@ -0,0 +1,43 @@
+window.onload = init;
+
+function init(){
+  let canvas = document.querySelector("#myCanvas");
+  let ctx = canvas.getContext("2d");
+  
+  let x = 20;
+  let y = 20;
+  
+  ctx.save();
+  ctx.fillStyle = "black";
+  //Head
+  ctx.beginPath();
+  ctx.arc(x+x/2, y+y/4, 12, 0, Math.PI*2);
+  ctx.stroke();
+  //Body
+  ctx.beginPath();
+  ctx.moveTo(x+x/2, y+y/4+12);
+  ctx.lineTo(x+x/2, y+y/4+50);
+  ctx.stroke();
+  //Feet
+  ctx.beginPath();
+  ctx.moveTo(x-5, y+y/4+50);
+  ctx.lineTo(x+25, y+y/4+50);
+  ctx.stroke();
+  //Left arm
+  ctx.beginPath();
+  ctx.moveTo(x+x/2, y+y/4+25);
+  ctx.lineTo(x+x/2-15, y+y/4+25);
+  ctx.stroke();
+  //right arm
+  ctx.beginPath();
+  ctx.moveTo(x+x/2, y+y/4+25);
+  ctx.lineTo(x+x/2+15, y+y/4+25);
+  ctx.stroke();
+  //Shield
+  ctx.beginPath();
+  ctx.moveTo(x+x/2-15, y+y/4+20);
+  ctx.lineTo(x+x/2+15, y+y/4+25);
+  ctx.stroke();
+  //Sword
+  ctx.restore();
+}
