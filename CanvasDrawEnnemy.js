028421d07c423ce8c3b09a9fc1385a85298c20d4
Gilles-Marco <36821430+Gilles-Marco@users.noreply.github.com>
2019-11-14 09:48:45 +0100

    Update CanvasDrawEnnemy.js

--- CanvasDrawEnnemy.js
+++ CanvasDrawEnnemy.js
@@ -12,7 +12,7 @@ function init(){
   //Head
   ctx.beginPath();
   ctx.arc(x+x/2, y+y/4, 12, 0, Math.PI*2);
-  ctx.stroke();
+  ctx.fill();
   //Body
   ctx.beginPath();
   ctx.moveTo(x+x/2, y+y/4+12);
@@ -42,6 +42,27 @@ function init(){
   ctx.lineTo(x+x/2-25, y+y/4+15);
   ctx.lineTo(x+x/2-15, y+y/4+15);
   ctx.fill();
-  //Sword
+  //Sword - Guard
+  ctx.beginPath();
+  ctx.moveTo(x+x/2+17, y+y/4+18);
+  ctx.lineTo(x+x/2+17, y+y/4+30);
+  ctx.lineTo(x+x/2+23, y+y/4+30);
+  ctx.lineTo(x+x/2+23, y+y/4+18);
+  ctx.lineTo(x+x/2+30, y+y/4+18);
+  ctx.lineTo(x+x/2+30, y+y/4+15);
+  ctx.lineTo(x+x/2+12, y+y/4+15);
+  ctx.lineTo(x+x/2+12, y+y/4+18);
+  ctx.lineTo(x+x/2+17, y+y/4+18);
+  ctx.fill();
+  //Sword - Blade
+  ctx.fillStyle="grey";
+  ctx.beginPath();
+  ctx.moveTo(x+x/2+17, y+y/4+15);
+  ctx.lineTo(x+x/2+17, y+y/4);
+  ctx.lineTo(x+x/2+20, y+y/4-5);
+  ctx.lineTo(x+x/2+23, y+y/4);
+  ctx.lineTo(x+x/2+23, y+y/4+15);
+  ctx.fill();
+  
   ctx.restore();
 }
