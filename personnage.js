c1f3693fa305ced9be164b935be94d783f4d81a1
Makusansu <maxencelefeuvre27@gmail.com>
2019-11-15 21:03:58 +0100

    Saut de joueur avec gravité en fonction du temps entre 2 frames et collision du joueur avec les bordures du canvas , fonctionnel !

--- personnage.js
+++ personnage.js
@@ -1,6 +1,7 @@
 //Constructeur de p{ersonnage
 export class Personnage{
     constructor(x1,y1,width,height,color,ctx){
+        this.vie = 3;
         this.x = x1;
         this.y = y1; 
         this.ctx = ctx;
@@ -32,13 +33,32 @@ export class Personnage{
         this.height=height;
         this.color=color;
       
-        //Variable de variations
-        this.dx = 1;
-        this.dy = 1;
-        console.log("Personnage créé");     
+        //Delta déplacements
+        this.dx = 0;
+        this.dy = 0;
+        console.log("Personnage créé en X :"+this.x +" Y :"+this.y);     
     }
   
     drawPersonnage(){
+        //variable de la tête
+        this.teteX = this.x;
+        this.teteY = this.y-30;
+      
+        //Variable du corps
+        this.corpsX = this.x;
+        this.corpsY = this.y-20;
+        this.angleCY = this.y;
+      
+        //Variable du bras
+        this.brasX = this.x;
+        this.brasY = this.y-10;
+        this.angleBY = this.y-10;
+      
+        //Variable jambe
+        this.jambeX = this.x;
+        this.jambeY = this.y;
+        this.angleJY = this.y+20;
+
         this.ctx.strokeStyle = this.color;
         this.ctx.beginPath();
 
@@ -62,32 +82,30 @@ export class Personnage{
     
         this.ctx.stroke();
         this.ctx.closePath();
-        console.log("Personnage dessiné");
     }
   
-  deplacePersonnage(deltaX, deltaY){
-  
-  //Mettre un +
-    //Test collision
-      this.x +=deltaX;
-      this.y += deltaY;
+  deplacePersonnage(){
+      this.x += this.dx;
+      this.y += this.dy;
     
-      this.teteX += deltaX;
-      this.corpsX += deltaX;
-      this.brasX += deltaX;
-      this.jambeX += deltaX;
-      this.angleCX += deltaX;
-      this.angleBX += deltaX;
-      this.angleJX1 += deltaX;
-      this.angleJX2 += deltaX;
+      //Deplacement en X
+      this.teteX += this.dx;
+      this.corpsX += this.dx;
+      this.brasX += this.dx;
+      this.jambeX += this.dx;
+      this.angleCX += this.dx;
+      this.angleBX += this.dx;
+      this.angleJX1 += this.dx;
+      this.angleJX2 += this.dx;
       
-      this.teteY += deltaY;
-      this.corpsY += deltaY;
-      this.angleCY += deltaY;
-      this.brasY += deltaY;
-      this.angleBY +=deltaY;
-      this.jambeY += deltaY;
-      this.angleJY += deltaY;
+      //Deplacement en Y
+      this.teteY += this.dy;
+      this.corpsY += this.dy;
+      this.angleCY += this.dy;
+      this.brasY += this.dy;
+      this.angleBY +=this.dy;
+      this.jambeY += this.dy;
+      this.angleJY += this.dy;
 
     }
 }
--- script.js
+++ script.js
@@ -1,20 +1,9 @@
 import {saveScore, getScoreList} from "./save.js";
-import { PlatformGenerator } from "./platform/plateformGenerator.js";
-import { Platform } from "./platform/plateforme.js";
-import { Ennemy } from "./ennemy/ennemy.js";
-import { EnnemyGenerator } from "./ennemy/ennemyGenerator.js";
+import {Personnage} from "./personnage.js";
 
 window.onload = init;
 
-//DEBUG Variable
-const DEBUG = true;
-
-//Game physics
-const gravite = 9.8;
-
-//GUI variable
 var canvas;
-var ctx;
 
 var startButton;
 var saveScoreDiv;
@@ -22,37 +11,32 @@ var saveScoreDivButton;
 var scoreScreenDiv;
 var scoreScreenButton;
 
-//Game variable
+var sol = 0;
+var perso;
+var ctx;
+
+
+var t1 = 1;
+var delta = 1;
+var gravite = 9.8;
 var score = 0;
-var t1 = null;
-
-//Init game world variable
-var platformArray = [];
-var arrowArray = [];
-var ennemyArray = [];
-
-//Player variable
-var player;
-
-//Constante sur la taille des plateformes
-const platformWidth = 120;
-const platformHeight = 30;
-var platformGenerator;
-
-//Ennemy variable
-var ennemyGenerator;
 
 function init(){
+  console.log("Page chargée");
+
   //Resize canvas to fullscreen
   canvas = document.querySelector("#myCanvas");
-  canvas.height = window.outerHeight;
-  canvas.width = window.outerWidth;
-  ctx = canvas.getContext("2d");
 
-  //Bind GUI button to action
+  ctx = canvas.getContext("2d");
+  sol = (canvas.clientHeight/20);
+
+  //Creation du personnage
+  perso = new Personnage(30, (canvas.clientHeight - sol),20,20,"blue",ctx);
+
+
+  //Bind button to action
   startButton = document.querySelector("#startButton");
-  if(DEBUG)
-    startButton.style.visibility = "hidden";
+  startButton.style.visibility = "hidden";
   startButton.onclick = startGame;
   
   saveScoreDiv = document.querySelector("#saveScore");
@@ -64,49 +48,92 @@ function init(){
   scoreScreenDiv.style.visibility = "hidden";
   scoreScreenButton = document.querySelector("#scoreScreen button");
   scoreScreenButton.onclick = newGame;
+  //display the original frame
 
-  //Générateur de platform
-  platformGenerator = new PlatformGenerator(20, platformWidth, platformHeight, 50, 5, platformArray, canvas, ctx);
-  //Générateur d'ennemis
-  ennemyGenerator = new EnnemyGenerator(150, ennemyArray, 0, 10);
+
+  //Listener pour le déplacement
+  window.addEventListener('keydown',function(event){
+    if(event.keyCode === 39 ){
+      perso.dx += 1  
+       this.console.log("Le perso avance à droite");
+    }
+    if(event.keyCode === 37 ){
+      perso.dx -= 1 ;
+      this.console.log("Le perso avance à gauche");
+    }
+    if(event.keyCode === 38 && perso.dy>=0){
+      perso.dy -= 10;
+      console.log(perso.dy);
+    }
+  });
+
+
+  window.addEventListener('keyup',function(event){
+    if(event.keyCode === 39){
+      perso.dx = 0;
+
+       this.console.log("Le perso avance à droite");
+    }
+    if(event.keyCode === 37){
+      perso.dx = 0; 
+
+      this.console.log("Le perso avance à gauche");
+    }
+
+
+  });
+
+
   updateCanvas();
+
 }
 
 function updateCanvas(timestamp){
-  //Main function loop
-  //Redraw the background
-  ctx.save();
-  ctx.fillStyle="lightgrey";
-  ctx.fillRect(0, 0, canvas.clientWidth, canvas.clientHeight);
-  ctx.restore();
-
   //Get time delta
-  let delta = 1;
-  if(t1==null){
-    t1 = timestamp;
-  }
-  else{
+  if(timestamp!=undefined){
     delta = timestamp-t1;
     t1 = timestamp;
   }
+
+  // 1 - Clear
+  ctx.clearRect(0,0, canvas.width, canvas.height);
+
+  // Affichage FPS
   drawFps(delta);
 
-  //Generation des plateformes
-  platformGenerator.generate();
-  //Draw Platform
-  platformArray.forEach((item, index)=>{
-    item.draw();
-  });
+  // 2 - Test des collision du joueur
+  playerCollision();
 
+  // 3 - Deplacement personnage
+  perso.deplacePersonnage();
+ 
+  
+   // 4 - Draw
+  perso.drawPersonnage();
+  
+  // 5 - Animation
   requestAnimationFrame(updateCanvas);
 }
+
+function playerCollision(){
+  perso.dy += gravite*delta/1000;
+  if(perso.y > (canvas.height - sol) && perso.dy>0){
+    perso.y = canvas.height - sol;
+    perso.dy = 0;
+  }
+ if(perso.x >= (canvas.width -20)&& perso.dx>0){
+    perso.dx = 0;
+  
+  }
+}
+
 
 function startGame(){
   //Onclick of the startbutton, launch the game
   startButton.style.visibility = "hidden";
-  // displaySaveScore();
+  displaySaveScore();
 
-  requestAnimationFrame(updateCanvas);
+  //requestAnimationFrame(updateCanvas);
 }
 
 function displaySaveScore(){
@@ -117,9 +144,9 @@ function handlerSaveScore(){
   //Onclick of "save" button for score, then print the score
   let pseudo = document.querySelector("#saveScore input[type=\"text\"]");
   console.log(pseudo.value);
-  
+  saveScoreDiv.style.visibility = "hidden";
+
   if(pseudo.value!=""){
-    saveScoreDiv.style.visibility = "hidden";
     saveScore(pseudo.value, score);
     pseudo.value = "";
     score = 0;
@@ -145,7 +172,7 @@ function newGame(){
 
 function drawFps(delta){
   let fps = 1000/delta;
-  fps = Math.round(fps);
+  let ctx = canvas.getContext("2d");
   ctx.save();
   ctx.font = "12px Arial";
   ctx.fillStyle = "red";
