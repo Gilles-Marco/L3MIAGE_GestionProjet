f0950921e9d0b61cbd636958339641f035f58bf8
Gilles-Marco <36821430+Gilles-Marco@users.noreply.github.com>
2019-11-20 08:21:35 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
 import {saveScore, getScoreList} from "./save.js";
+import { PlatformGenerator } from "./platform/plateformGenerator.js";
+import { Platform } from "./platform/plateforme.js";
+import { Ennemy } from "./ennemy/ennemy.js";
+import { EnnemyGenerator } from "./ennemy/ennemyGenerator.js";
 import {Personnage} from "./personnage.js";
 
 window.onload = init;
 
+//DEBUG Variable
+const DEBUG = true;
+
+//Game physics
+const gravite = 9.8;
+
+//GUI variable
 var canvas;
+var ctx;
 
 var startButton;
 var saveScoreDiv;
@@ -11,10 +23,28 @@ var saveScoreDivButton;
 var scoreScreenDiv;
 var scoreScreenButton;
 
+//Game variable
+var score = 0;
+var t1 = null;
+
+//Init game world variable
+var platformArray = [];
+var arrowArray = [];
+var ennemyArray = [];
+
+//Player variable
+var player;
 var sol = 0;
 var perso;
 var ctx;
 
+//Constante sur la taille des plateformes
+const platformWidth = 120;
+const platformHeight = 30;
+var platformGenerator;
+
+//Ennemy variable
+var ennemyGenerator;
 
 var t1 = 1;
 var delta = 1;
@@ -26,6 +56,8 @@ function init(){
 
   //Resize canvas to fullscreen
   canvas = document.querySelector("#myCanvas");
+  canvas.height = window.outerHeight;
+  canvas.width = window.outerWidth;
 
   ctx = canvas.getContext("2d");
   sol = (canvas.clientHeight/20);
@@ -36,7 +68,8 @@ function init(){
 
   //Bind button to action
   startButton = document.querySelector("#startButton");
-  startButton.style.visibility = "hidden";
+  if(DEBUG)
+    startButton.style.visibility = "hidden";
   startButton.onclick = startGame;
   
   saveScoreDiv = document.querySelector("#saveScore");
@@ -55,7 +88,7 @@ function init(){
   window.addEventListener('keydown',function(event){
     if(event.keyCode === 39 ){
       perso.dx += 1  
-       this.console.log("Le perso avance à droite");
+      this.console.log("Le perso avance à droite");
     }
     if(event.keyCode === 37 ){
       perso.dx -= 1 ;
@@ -83,23 +116,37 @@ function init(){
 
   });
 
-
+  //Générateur de platform
+  platformGenerator = new PlatformGenerator(20, platformWidth, platformHeight, 50, 5, platformArray, canvas, ctx);
+  //Générateur d'ennemis
+  ennemyGenerator = new EnnemyGenerator(150, ennemyArray, 0, 10);
   updateCanvas();
 
 }
 
 function updateCanvas(timestamp){
+  //Main function loop
+  //Redraw the background
+  ctx.save();
+  ctx.fillStyle="lightgrey";
+  ctx.fillRect(0, 0, canvas.clientWidth, canvas.clientHeight);
+  ctx.restore();
+  
   //Get time delta
   if(timestamp!=undefined){
     delta = timestamp-t1;
     t1 = timestamp;
   }
 
-  // 1 - Clear
-  ctx.clearRect(0,0, canvas.width, canvas.height);
-
   // Affichage FPS
   drawFps(delta);
+  
+  //Generation des plateformes
+  platformGenerator.generate();
+  //Draw Platform
+  platformArray.forEach((item, index)=>{
+    item.draw();
+  });
 
   // 2 - Test des collision du joueur
   playerCollision();
@@ -131,9 +178,9 @@ function playerCollision(){
 function startGame(){
   //Onclick of the startbutton, launch the game
   startButton.style.visibility = "hidden";
-  displaySaveScore();
+  //displaySaveScore();
 
-  //requestAnimationFrame(updateCanvas);
+  requestAnimationFrame(updateCanvas);
 }
 
 function displaySaveScore(){
@@ -144,9 +191,9 @@ function handlerSaveScore(){
   //Onclick of "save" button for score, then print the score
   let pseudo = document.querySelector("#saveScore input[type=\"text\"]");
   console.log(pseudo.value);
-  saveScoreDiv.style.visibility = "hidden";
 
   if(pseudo.value!=""){
+    saveScoreDiv.style.visibility = "hidden";
     saveScore(pseudo.value, score);
     pseudo.value = "";
     score = 0;
@@ -172,7 +219,7 @@ function newGame(){
 
 function drawFps(delta){
   let fps = 1000/delta;
-  let ctx = canvas.getContext("2d");
+  fps = Math.round(fps);
   ctx.save();
   ctx.font = "12px Arial";
   ctx.fillStyle = "red";
